8ca366a1caaa9d6e5182b76173cb98282e9b0c27
IbragimKhaniev <newinterny@gmail.com>
2025-11-27 09:38:29 +0300

    fix: add missing getUserById export to users API

--- react/src/api/users.js
+++ react/src/api/users.js
@@ -5,6 +5,11 @@ export const getUser = async (userId) => {
   return response.data;
 };
 
+export const getUserById = async (userId) => {
+  const response = await axios.get(`/api/users/${userId}/`);
+  return response.data;
+};
+
 export const updateUser = async (userId, userData) => {
   const response = await axios.put(`/api/users/${userId}/`, userData);
   return response.data;
